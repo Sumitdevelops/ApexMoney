c5ed5b71d543f16ec9ac41d42fbca0107991fbc4
Sumit <sumsri2024@gmail.com>
2025-10-11 22:08:09 +0530

    cors origin updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend's URL
+  origin: ["http://localhost:5173","https://apexmoney.netlify.app"],// Replace with your frontend's URL
   credentials: true,
 };
 
