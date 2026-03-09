4e1f59b89b2e9bf23f1049785e1b70c84f555ed1
Sumit <sumsri2024@gmail.com>
2026-03-09 16:16:48 +0530

    Fix: Add SMTP timeouts to prevent hanging email connections
    - Add connectionTimeout: 10s, greetingTimeout: 10s, socketTimeout: 15s
    - Prevents sendMail from hanging indefinitely on slow SMTP connections

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,6 +19,9 @@ const getTransporter = () => {
          user: process.env.EMAIL_USER,
          pass: process.env.EMAIL_PASS,
       },
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 15000,
    });
 
    // Verify connection on first creation
